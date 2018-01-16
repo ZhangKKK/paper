e676b63e1fb78aa4448f811862c22a894c7d9555
zhangkkk <jinffyso@qq.com>
2018-01-16 16:24:17 +0800

    change

--- paper.py
+++ paper.py
@@ -15,7 +15,6 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-
 class Segmenter(nn.Module):
     def __init__(self):
         super(Segmenter, self).__init__()
@@ -35,26 +34,26 @@ class Segmenter(nn.Module):
                                              nn.Conv3d(40, 40, 3))
         self.res_low_3 = nn.Sequential(nn.Conv3d(40, 50, 3),
                                              nn.Conv3d(50, 50, 3))
-        self.fc_path_1 = nn.Sequential(nn.Linear(100 * 9 * 9 * 9, 150 * 9 * 9 * 9),
-                                     nn.Linear(150 * 9 * 9 * 9, 150 * 9 * 9 * 9))
-        self.fc_path_2 = nn.Linear(150 * 9 * 9 * 9, 2 * 9 * 9 * 9)
+        self.res_1 = nn.Sequential(nn.Conv3d(100, 150, 1),
+                                     nn.Conv3d(150, 150, 1))
+        self.res_2 = nn.Conv3d(150, 2, 1)
         
-    def forward(self, x, y, alpha):
+    def forward(self, x, y):
         
         mm = nn.Upsample(scale_factor = 2, mode='nearest')
         N = x.size(0)
         x_normal_1 = self.res_normal_1(x)
-        x_normal_c1 = x_normal_1[:, :9, :9, :9]
+        x_normal_c1 = x_normal_1[:, 4: 13, 4: 13, 4: 13]
         x_normal_2 = self.res_normal_2(x_normal_1)
-        x_normal_c2 = x_normal_2[:, :9, :9, :9]
+        x_normal_c2 = x_normal_2[:, 2: 11, 2: 11, 2: 11]
         x_normal_3 = self.res_normal_3(x_normal_2)
         m = nn.Upsample(scale_factor = 3, mode='nearest')
         x_low_1 = self.res_low_1(y)
         x_low_up_1 = mm(x_low_1)
-        x_low_c1 = x_low_up_1[:, :9, :9, :9]
+        x_low_c1 = x_low_up_1[:, 7: 16 , 7: 16, 7: 16]
         x_low_2 = self.res_low_2(x_low_1)
         x_low_up_2 = mm(x_low_2)
-        x_low_c2 = x_low_up_2[:, :9, :9, :9]
+        x_low_c2 = x_low_up_2[:, 2: 11, 2: 11, 2: 11]
         x_low_3 = self.res_low_3(x_low_2)
         x_low_c3 = m(x_low_3)
         
@@ -64,11 +63,11 @@ class Segmenter(nn.Module):
         
         conc = torch.cat(x_normal_3, x_low)
         N = conc.size(0) 
-        out_1 = out.view(N, -1)       
-        out_2 = self.fc_path_1(out_1)
-        concat = torch.cat(x_low_c1, x_low_c2, x_low_c3, x_normal_c1, x_normal_c2, x_normal_3, out_2)
-        out_3 = self.fc_path_2(out_2)
-        out = out_3.view(N, 2, 9, 9, 9)
+           
+        out1 = self.res_1(conc)
+        concat = torch.cat(x_low_c1, x_low_c2, x_low_c3, x_normal_c1, x_normal_c2, x_normal_3, out1)
+        out = self.res_2(out1)
+
         
         return out, concat
 
@@ -87,6 +86,8 @@ def discriminator():
     return model
 
 
+
+
 def bce_loss(input, target):
     neg_abs = - input.abs()
     loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
@@ -101,7 +102,7 @@ def discriminator_loss(h_real, h_fake):
     return loss
 
 
-def segmenter_loss(y_predict, y_true, alpha, h_real, h_fake, alpha):
+def segmenter_loss(y_predict, y_true, h_real, h_fake, alpha):
     y_predit = y_predict.view(y_predict.size(0) * 2 * 9 * 9 *9, -1)
     y_true = y_true.view(y_true.size(0) * 2 * 9 * 9 * 9, -1)
     loss1 = bce_loss(y_predict, y_true)
@@ -118,7 +119,6 @@ def get_optimizer(model):
     optimizer = None
     optimizer = optim.SGD(model.parameters(), lr = 0.001)
     return optimizer
-
 
 
 
@@ -148,9 +148,7 @@ def gan(D, S, D_solver, S_solver, discriminator_loss, segmenter_loss, show_every
             x_high = Variable(x_high)
             x_s_high = Variable(x_s_high)
             x_t_high = Variable(x_t_high)
-            
-            D_solver.zero_grad()
-            
+            S_solver.zero_grad()
             
             y_predict,  _ = S(x_high, x)
             _, out_real = S(x_s_high, x_s)
@@ -160,34 +158,31 @@ def gan(D, S, D_solver, S_solver, discriminator_loss, segmenter_loss, show_every
             h_real = D(out_real)
             h_fake = D(out_fake)
             
-            D_error = discriminator_loss(h_real, h_fake)
-            
-            D_error.backward()
-            D_solver.step()
-            
-            h_real = D(out_real)
-            h_fake = D(out_fake)
-            
             S_solver.zero_grad()
-            
             if epoch < 10:
                 
-                S_error = segmenter_loss(y_predict, y_true, alpha, h_real, h_fake, 0)
+                S_error = segmenter_loss(y_predict, y_true, h_real, h_fake, 0)
                 S_error.backward()
                 S_solver.step()
                 
             else if epoch < 35:
-                S_error = segmenter_loss(y_predict, y_true, alpha, h_real, h_fake, 0.05 * (epoch - 9) / (34 - 9))
+                S_error = segmenter_loss(y_predict, y_true, h_real, h_fake, 0.05 * (epoch - 9) / (34 - 9))
                 S_error.backward()
                 S_solver.step()
                 
             else:
-                S_error = segmenter_loss(y_predict, y_true, alpha, h_real, h_fake, 0.05)
+                S_error = segmenter_loss(y_predict, y_true, h_real, h_fake, 0.05)
                 S_error.backward()
                 S_solver.step()
             
+           
+            D_solver.zero_grad()
+            D_error = discriminator_loss(h_real, h_fake)
+            D_error.backward()
+            D_solver.step()
+
             if (iter_count % show_every == 0):
-                print('Iter: {}, D: {:.4}, G:{:.4}'.format(iter_count, D_error.data[0], G_error.data[0]))
+                print('Iter: {}, D: {:.4}, G:{:.4}'.format(iter_count, D_error.data[0], S_error.data[0]))
                
             iter_count += 1
             
@@ -196,3 +191,20 @@ def to_high_res(x):
     mm = nn.Upsample(scale_factor = 2, mode='nearest')
     out = mm(out)[:, :25, :25, :25]
     return out
+
+
+
+
+
+
+
+D = discriminator()
+S = Segmenter()
+
+D_solver = get_optimizer(D)
+S_solver = get_optimizer(S)
+
+gan(D, S, D_solver, S_solver, discriminator_loss, segmenter_loss)
+
+
+
